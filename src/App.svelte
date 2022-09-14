a10d31021053c00e082ecdf5ac6a01f15fd5b52d
Joe Hsu <jhsu.x1@gmail.com>
2022-09-14 15:08:35 +0000

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+
+</script>
+
+<main>
+  <h1>Vite + Svelte</h1>
+
+  <p>
+    Svelte and vite
+  </p>
+
+  <p class="read-the-docs">
+    here is some text
+  </p>
+</main>
+
+<style>
+  .read-the-docs {
+    color: #888;
+  }
+</style>
